529e7fded65d506a8b32385876eefcab57ddbca5
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-04 09:01:39 +0900

    init: initial spring-boot setting

---
+++ src/main/java/com/steady/leisurethatapi/LeisurethatApiApplication.java
@@ -0,0 +1,13 @@
+package com.steady.leisurethatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeisurethatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeisurethatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steady/leisurethatapi/LeisurethatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steady.leisurethatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeisurethatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
