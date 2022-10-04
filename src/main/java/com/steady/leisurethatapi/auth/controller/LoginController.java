9d0f8b9eb87268d1de3a894d143a73af1699a979
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-04 14:59:28 +0900

    feature: implement Login

---
+++ src/main/java/com/steady/leisurethatapi/auth/controller/LoginController.java
@@ -0,0 +1,67 @@
+package com.steady.leisurethatapi.auth.controller;
+
+import com.steady.leisurethatapi.auth.dto.LoginDTO;
+import com.steady.leisurethatapi.auth.util.JwtUtil;
+import com.steady.leisurethatapi.common.dto.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public LoginController(JwtUtil jwtUtil, AuthenticationManager authenticationManager) {
+        this.jwtUtil = jwtUtil;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginInfo){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
+        Map<String,Object> responseMap = new HashMap<>();
+
+        String token = null;
+        Boolean selectCategory = false;
+
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginInfo.getUserName(),
+                            loginInfo.getPassword()
+                    )
+            );
+        } catch (Exception e){
+            System.out.println(e);
+            responseMap.put("inputId", loginInfo.getUserName());
+            return ResponseEntity
+                    .badRequest()
+                    .headers(headers)
+                    .body(new ResponseMessage(400, "login failed", responseMap));
+        }
+
+        token = jwtUtil.generateToken(loginInfo.getUserName());
+        responseMap.put("id",loginInfo.getUserName());
+        responseMap.put("token",token);
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(new ResponseMessage(200, "token generated", responseMap));
+    }
+
+}
---
+++ src/main/java/com/steady/leisurethatapi/auth/dto/LoginDTO.java
@@ -0,0 +1,13 @@
+package com.steady.leisurethatapi.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    private String userName;
+    private String password;
+}
