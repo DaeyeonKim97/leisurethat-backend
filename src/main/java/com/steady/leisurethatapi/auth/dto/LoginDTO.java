3434ea49ce948543c7fe0f4bbc45249403ae686c
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-04 16:06:56 +0900

    refactor: userName을 username으로 변경

--- src/main/java/com/steady/leisurethatapi/auth/dto/LoginDTO.java
+++ src/main/java/com/steady/leisurethatapi/auth/dto/LoginDTO.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginDTO {
-    private String userName;
+    private String username;
     private String password;
 }
