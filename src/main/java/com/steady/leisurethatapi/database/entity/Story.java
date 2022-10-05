c65faec98f14cbaeb8f838727084f0cbe8b55518
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-05 10:58:36 +0900

    feature: story entity 구현

---
+++ src/main/java/com/steady/leisurethatapi/database/entity/Story.java
@@ -0,0 +1,45 @@
+package com.steady.leisurethatapi.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Blob;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TBL_STORY")
+@SequenceGenerator(
+        name = "SEQ_STORY_ID_GENERATOR",
+        sequenceName = "SEQ_STORY_ID"
+)
+public class Story {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "SEQ_STORY_ID_GENERATOR"
+    )
+    @Column(name = "STORY_ID")
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "PROJECT_ID")
+    private Project project;
+    @Column(name = "STORY_TITLE")
+    private String title;
+    @Lob
+    @Column(name = "STORY_CONTENT")
+    private Blob content;
+    @Column(name = "STORY_VIEWS")
+    private int views;
+    @Column(name = "STORY_DATE")
+    private Date date;
+    @Column(name = "STORY_YN")
+    private String yN;
+    @OneToOne
+    @JoinColumn(name = "ATC_ID")
+    private Attachment attachment;
+}
