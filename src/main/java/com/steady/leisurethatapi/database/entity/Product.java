3e2f6f74ac0c40551b8bf4085591659bbf18a965
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-05 10:58:19 +0900

    feature: product 관련 entity 구현

---
+++ src/main/java/com/steady/leisurethatapi/database/entity/Product.java
@@ -0,0 +1,35 @@
+package com.steady.leisurethatapi.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="TBL_PRODUCT")
+@SequenceGenerator(
+        name = "SEQ_PRODUCT_ID_GENERATOR",
+        sequenceName = "SEQ_PRODUCT_ID",
+        allocationSize = 1
+)
+public class Product {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "SEQ_PRODUCT_ID_GENERATOR"
+    )
+    @Column(name = "PRODUCT_ID")
+    private int id;
+    @Column(name = "PRODUCT_NAME")
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "PROJECT_ID")
+    private Project project;
+    @OneToOne
+    @JoinColumn(name = "ATC_ID")
+    private Attachment attachment;
+}
