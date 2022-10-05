ef30221d763cfe5cc10778a28694a6c7d7c55238
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-05 12:47:56 +0900

    feature: Reward JPA repository 구현

---
+++ src/main/java/com/steady/leisurethatapi/database/repository/RewardRepository.java
@@ -0,0 +1,8 @@
+package com.steady.leisurethatapi.database.repository;
+
+import com.steady.leisurethatapi.database.entity.Reward;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RewardRepository extends JpaRepository<Reward, Integer> {
+
+}
