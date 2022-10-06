95c18b073310e12a114c2deecf7cd0e0a4bebe40
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-06 14:48:44 +0900

    fix: aws 주석 제거

--- src/main/java/com/steady/leisurethatapi/attachment/service/UploadS3Service.java
+++ src/main/java/com/steady/leisurethatapi/attachment/service/UploadS3Service.java
@@ -1,64 +1,63 @@
-//package com.steady.leisurethatapi.attachment.service;
-//
-//import com.amazonaws.services.s3.AmazonS3Client;
-//import com.amazonaws.services.s3.model.CannedAccessControlList;
-//import com.amazonaws.services.s3.model.ObjectMetadata;
-//import com.amazonaws.services.s3.model.PutObjectRequest;
-//import com.steady.leisurethatapi.database.entity.Attachment;
-//import com.steady.leisurethatapi.database.repository.AttachmentRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.util.Date;
-//import java.util.UUID;
-//
-//@Service
-//public class UploadS3Service {
-//    private AmazonS3Client amazonS3Client;
-//    private final AttachmentRepository attachmentRepository;
-//    @Value("${cloud.aws.s3.bucket}")
-//    private String bucketName;
-//
-//    @Autowired
-//    public UploadS3Service( AttachmentRepository attachmentRepository) {
-//        this.attachmentRepository = attachmentRepository;
-//        System.out.println("bucket-name : " + bucketName);
-//    }
-//
-//    public String uploadFile(String category, MultipartFile multipartFile) {
-//
-//        validateFileExists(multipartFile);
-//
-//        String originalFilename = multipartFile.getOriginalFilename();
-//        String ext = originalFilename.substring(originalFilename.lastIndexOf(".")+1);
-//        String fileName = category+ UUID.randomUUID() + "." + ext;
-//
-//
-//
-//        ObjectMetadata objectMetadata = new ObjectMetadata();
-//        objectMetadata.setContentType(multipartFile.getContentType());
-//
-//        try (InputStream inputStream = multipartFile.getInputStream()) {
-//            amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
-//                    .withCannedAcl(CannedAccessControlList.PublicRead));
-//            Attachment attachment = new Attachment(0,originalFilename,fileName,"https://leisurethat-image.s3.ap-northeast-2.amazonaws.com/"+fileName,new Date());
-//            attachmentRepository.save(attachment);
-//        } catch (IOException e) {
-//            System.out.println("upload s3 error");
-//            throw new RuntimeException();
-//        }
-//
-//        return amazonS3Client.getUrl(bucketName, fileName).toString();
-//    }
-//
-//    private void validateFileExists(MultipartFile multipartFile) {
-//        if (multipartFile.isEmpty()) {
-//            System.out.println("file is empty");
-//            throw new RuntimeException();
-//        }
-//    }
-//}
+package com.steady.leisurethatapi.attachment.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.steady.leisurethatapi.database.entity.Attachment;
+import com.steady.leisurethatapi.database.repository.AttachmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class UploadS3Service {
+    private AmazonS3Client amazonS3Client;
+    private final AttachmentRepository attachmentRepository;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Autowired
+    public UploadS3Service( AttachmentRepository attachmentRepository) {
+        this.attachmentRepository = attachmentRepository;
+    }
+
+    public String uploadFile(String category, MultipartFile multipartFile) {
+
+        validateFileExists(multipartFile);
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String ext = originalFilename.substring(originalFilename.lastIndexOf(".")+1);
+        String fileName = category+ UUID.randomUUID() + "." + ext;
+
+
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            Attachment attachment = new Attachment(0,originalFilename,fileName,"https://leisurethat-image.s3.ap-northeast-2.amazonaws.com/"+fileName,new Date());
+            attachmentRepository.save(attachment);
+        } catch (IOException e) {
+            System.out.println("upload s3 error");
+            throw new RuntimeException();
+        }
+
+        return amazonS3Client.getUrl(bucketName, fileName).toString();
+    }
+
+    private void validateFileExists(MultipartFile multipartFile) {
+        if (multipartFile.isEmpty()) {
+            System.out.println("file is empty");
+            throw new RuntimeException();
+        }
+    }
+}
