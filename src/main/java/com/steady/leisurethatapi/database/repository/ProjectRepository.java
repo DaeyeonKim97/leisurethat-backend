682b08c0d8998da7681f9ed44de8e97cab39311d
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-09 22:36:09 +0900

    feature: 유저정보로 프로젝트 리스트 조회 메소드 추가

--- src/main/java/com/steady/leisurethatapi/database/repository/ProjectRepository.java
+++ src/main/java/com/steady/leisurethatapi/database/repository/ProjectRepository.java
@@ -3,6 +3,9 @@ package com.steady.leisurethatapi.database.repository;
 import com.steady.leisurethatapi.database.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     public Project findById(int id);
+    public List<Project> findByBusinessInfoMemberUsername(String username);
 }
