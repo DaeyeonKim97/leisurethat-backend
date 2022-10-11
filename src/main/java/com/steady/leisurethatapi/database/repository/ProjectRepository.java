c9b52539f57f61aab6757dab7818c210fd3ba0af
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-11 09:59:47 +0900

    feature: 프로젝트 상태로 프로젝트 리스트 조회 메소드 추가

--- src/main/java/com/steady/leisurethatapi/database/repository/ProjectRepository.java
+++ src/main/java/com/steady/leisurethatapi/database/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.steady.leisurethatapi.database.repository;
 
 import com.steady.leisurethatapi.database.entity.Project;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     public Project findById(int id);
     public List<Project> findByBusinessInfoMemberUsername(String username);
+    public List<Project> findByStatusId(int statusId, Pageable pageable);
 }
